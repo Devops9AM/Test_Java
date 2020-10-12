2e1266223d9bed60c3d880755588ca1f7df25b96
root <root@localhost.localdomain>
2020-10-12 19:01:13 +0530

    demo.java V1.0

---
+++ demo.java
@@ -0,0 +1,14 @@
+class demo
+{
+public static void main(String[] args)
+{
+int num=10, count=1, total=0;
+while(count<=num)
+{
+ total=total+count;
+  count++;
+}
+System.out.println("The Sum of First 10 Natural numbers are"+total);
+}
+}
+
